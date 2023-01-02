4b2903152691ebc49f9205607a5a6420c3a72a6f
Evelyn Rafaela Suquinagua Loja <rafaela.suquinagua@ucuenca.edu.ec>
2023-01-02 00:51:23 -0500

    visualizar celdas colapsadas

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 const celdas = []; //4x4
-const RETICULA = 4;
+const RETICULA = 8;
+let ancho; //altura de celda
+let alto; //anchura de celda
 
 const azulejos = [];
 const NA = 16; //numero de azulejos
@@ -149,6 +151,9 @@ function preload() {
 function setup() {
   createCanvas(1080, 1080);
 
+  ancho = width / RETICULA;
+  alto = height / RETICULA;
+
   let opcionesI = [];
   for (let i = 0; i < azulejos.length; i++) {
     opcionesI.push(i);
@@ -160,8 +165,47 @@ function setup() {
       opciones: opcionesI,
     }
   }
-  print(celdas);
+
 
 }
 
-function draw() { }
+function draw() {
+  background(111);
+  const celdasDisponibles = celdas.filter((celda) => {
+    return celda.colapsada == false;
+  });
+  if (celdasDisponibles.length > 0) {
+    celdasDisponibles.sort((a, b) => {
+      return a.opciones.length - b.opciones.length;
+    });
+
+    const celdasPorColapsar = celdasDisponibles.filter((celda) => {
+      return (
+        celda.opciones.length == celdasDisponibles[0].opciones.length
+      );
+    });
+
+    const celdaSeleccionada = random(celdasPorColapsar);
+    celdaSeleccionada.colapsada = true;
+
+    const opcionSeleccionada = random(celdaSeleccionada.opciones);
+    celdaSeleccionada.opciones = [opcionSeleccionada];
+
+
+    print(celdaSeleccionada);
+
+
+    for (let x = 0; x < RETICULA; x++) {
+      for (let y = 0; y < RETICULA; y++) {
+        const celdaIndex = x + y * RETICULA;
+        const celdaActual = celdas[celdaIndex];
+        if (celdaActual.colapsada) {
+          image(azulejos[celdaActual.opciones[0]],
+            x * ancho,
+            y * alto, ancho, alto);
+        }
+      }
+    }
+    //noLoop();
+  }
+}
